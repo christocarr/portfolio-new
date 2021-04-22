a69317052effb1b74a9ead060b0d1c96655e2314
christocarr <christocarr@outlook.com>
2021-04-22 14:58:48 +0100

    feat: add projects page

---
+++ pages/projects.js
@@ -0,0 +1,28 @@
+import Layout from '../components/layout/Layout';
+import ProjectCard from '../components/project-card/ProjectCard';
+import { getAllProjects } from '../lib/projects';
+
+export async function getStaticProps() {
+  const allProjects = await getAllProjects();
+  return {
+    props: {
+      allProjects,
+    },
+  };
+}
+
+export default function Projects({ allProjects }) {
+  console.log(allProjects);
+  return (
+    <Layout>
+      <h2>Projects</h2>
+      <ul>
+        {allProjects.map(({ fields, sys }) => (
+          <li key={sys.id}>
+            <ProjectCard fields={fields} />
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
