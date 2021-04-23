7283e83e8b719481938e2e15bdd569b455d0a678
christocarr <christocarr@outlook.com>
2021-04-23 12:56:53 +0100

    wip: add header component

---
+++ components/header/Header.js
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <div></div>
+      <button>
+        <span className={styles.menu}></span>
+      </button>
+    </header>
+  );
+}
